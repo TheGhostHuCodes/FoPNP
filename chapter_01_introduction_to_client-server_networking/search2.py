bbabcc710b7cd3687172cc0c392d11f15074217a
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-11-24 21:46:37 -0800

    Use Google Geocoding API to request a JSON document with lat/lng

---
+++ chapter_01_introduction_to_client-server_networking/search2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import requests
+
+
+def geocode(address):
+    parameters = {'address': address, 'sensor': 'false'}
+    base = 'http://maps.googleapis.com/maps/api/geocode/json'
+    response = requests.get(base, params=parameters)
+    answer = response.json()
+    print(answer['results'][0]['geometry']['location'])
+
+
+if __name__ == '__main__':
+    geocode("140 New Montgomery, San Francisco, CA")
